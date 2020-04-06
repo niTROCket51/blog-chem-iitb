253e90ffe005a50dfcdd1e9c745104f88532efe8
niTROCket51 <niket5151@gmail.com>
2020-04-06 23:33:29 +0530

    Add post details in admin.py

--- feed/admin.py
+++ feed/admin.py
@@ -3,4 +3,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'published_date']
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
